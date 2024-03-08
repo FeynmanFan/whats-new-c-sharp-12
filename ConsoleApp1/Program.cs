2bd1b6a0ff481ecee42f14398e01d909526ceafc
FeynmanFan <cbehrens@gmail.com>
2024-03-08 15:26:54 -0600

    3. Collection Expressions - initialization format

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,11 +4,33 @@
     {
         static void Main(string[] args)
         {
-            var a = new RocketA("Monster", 16);
-            var b = new RocketB("Mini", 2);
+            var monster = new RocketA("Monster", 16);
+            var mini = new RocketB("Mini", 2);
+            var dragon = new RocketA("Dragon", 12);
+            var wombat = new RocketB("Wombat", 3);
 
-            Console.WriteLine($"{a.Model} has {a.EngineCount} engines");
-            Console.WriteLine($"{b.Model} has {b.EngineCount} engines");
+            var rocketsA = new List<RocketA> { monster, dragon };
+
+            List<RocketA> rocketsA2 = new() { monster, dragon };
+
+            List<RocketB> rocketsB = [mini, wombat];
+
+
+            foreach (RocketA rocket in rocketsA)
+            {
+                Console.WriteLine(rocket.ToString());
+            }
+
+            foreach (RocketA rocket in rocketsA2)
+            {
+                Console.WriteLine(rocket.ToString());
+            }
+
+            foreach (RocketB rocket in rocketsB)
+            {
+                Console.WriteLine(rocket.ToString());
+            }
+
         }
     }
 }
